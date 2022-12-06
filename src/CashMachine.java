a038b40fdb8dd0f6b526c2899d1fb995beb752be
Arina <arina.milyutina.0005@gmail.com>
2022-12-06 17:12:56 +0300

    commit1

--- src/CashMachine.java
+++ src/CashMachine.java
@@ -4,53 +4,41 @@ public class CashMachine {
     public int countHundred;
     public int allMoney;
 
-    CashMachine(int countFifty, int countHundred, int countTwenty, int allMoney) {
+    CashMachine(int countFifty, int countHundred, int countTwenty) {
         this.countFifty = countFifty;
         this.countHundred = countHundred;
         this.countTwenty = countTwenty;
-        this.allMoney = allMoney;
     }
 
-    public void addMoney(int addTwenties, int addFifties, int addHundreds) {
-        countTwenty += addTwenties;
-        countFifty += addFifties;
-        countHundred += addHundreds;
-        int allAdd = 20 * addTwenties + 50 * addFifties + 100 * addHundreds;
-        allMoney = allAdd + allMoney;
-        System.out.println("The number of banknotes after adding money:\nCount 20 banknote: " + countTwenty + ";\nCount 50 banknote: " + countFifty + ";\nCount 100 banknote: " + countHundred
-                + ";\nAll money: " + allMoney + " BYN;\n");
+    public void addMoney(int cash) {
+        this.allMoney = 20 * countTwenty + 50 * countFifty + 100 * countHundred;
+        allMoney += cash;
     }
 
-    public boolean takeAwayMoney(int count20, int count50, int count100) {
-        int sumOfTakeAwayMoney = 20 * count20 + 50 * count50 + 100 * count100;
-        if (allMoney >= sumOfTakeAwayMoney) {
-            allMoney = allMoney - sumOfTakeAwayMoney;
-            countTwenty = countTwenty - count20;
-            countFifty = countFifty - count50;
-            countHundred += countHundred - count100;
-            System.out.println("Before withdrawal of money:\n" + "All money: " + allMoney + " BYN;");
-            System.out.println("Count 20 banknote: " + countTwenty + ";\nCount 50 banknote: " + countFifty + ";\nCount 100 banknote: " + countHundred+";");
+    public boolean takeAwayMoney(int cash) {
+        if (allMoney >= cash) {
+            System.out.println("The operation was successful:)");
             return true;
         } else {
-            System.out.println("Not enough money :(");
+            System.out.println("Not enough money:(");
             return false;
         }
     }
 
     @Override
     public String toString() {
-        return "Before starting work CashMachine:\n" +
-                "Count 20 banknote: " + countTwenty +
-                ";\nCount 50 banknote: " + countFifty +
-                ";\nCount 100 banknote: " + countHundred +
-                ";\nAll money: " + allMoney + " BYN" +
-                ";\n";
+        return "CashMachine:\n" +
+                "countTwenty=" + countTwenty +
+                ",\ncountFifty=" + countFifty +
+                ",\ncountHundred=" + countHundred +
+
+                ';';
     }
 
     public static void main(String[] args) {
-        CashMachine atm = new CashMachine(10, 20, 30, 3100);
+        CashMachine atm = new CashMachine(10, 20, 30);
         System.out.println(atm);
-        atm.addMoney(100, 40, 50);
-        atm.takeAwayMoney(5, 10, 6);
+        atm.addMoney(4000);
+        atm.takeAwayMoney(100000);
     }
 }
